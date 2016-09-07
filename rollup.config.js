9e2065ef17552f27a7ec4829431612dfa2885ecf
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-09-07 12:05:37 +0100

    Setup initial build and test infrastructure.

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import babel from 'rollup-plugin-babel'
+import babelrc from 'babelrc-rollup'
+import postcss from 'rollup-plugin-postcss'
+
+export default {
+  entry: 'src/index.js'
+, dest: 'dist/grid-components.js'
+, format: 'umd'
+, moduleName: 'gridComponents'
+, external: [
+    '@zambezi/d3-utils'
+  , 'd3-dispatch'
+  , 'd3-selection'
+  ]
+, sourceMap: true
+, plugins: [
+    postcss(
+      {
+        plugins: [ ]
+      , extensions: ['.css', '.sss']
+      }
+    )
+  , babel(babelrc())
+  ]
+, globals: {
+    '@zambezi/d3-utils': 'd3Utils'
+  , 'd3-dispatch': 'd3'
+  , 'd3-selection': 'd3'
+  }
+}
