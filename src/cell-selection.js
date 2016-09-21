897212fc2a360fcb3a88de24d9cbc7ad4dbff446
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-09-21 17:46:26 +0100

    Stub selected change API.

--- src/cell-selection.js
+++ src/cell-selection.js
@@ -1,9 +1,15 @@
+import { dispatch  as createDispatch }  from 'd3-dispatch'
+import { rebind } from '@zambezi/d3-utils'
+
 export function createCellSelection() {
+
+  const dispatch = createDispatch('cell-selected-change')
+
   function cellSelection(s) {
     s.each(cellSelectionEach)
   }
 
-  return cellSelection
+  return rebind().from(dispatch, 'on')(cellSelection)
 
   function cellSelectionEach(d, i) {
     console.debug('cellSelectionEach', d, this)
