a1ad2240b1994b2aad7d7b8a6b082e4a51340067
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-09-22 11:54:50 +0100

    Stub selection display.

--- src/cell-selection.js
+++ src/cell-selection.js
@@ -1,17 +1,53 @@
 import { dispatch  as createDispatch }  from 'd3-dispatch'
-import { rebind } from '@zambezi/d3-utils'
+import { rebind, forward } from '@zambezi/d3-utils'
+import { select } from 'd3-selection'
+
+import './cell-selection.css'
 
 export function createCellSelection() {
 
   const dispatch = createDispatch('cell-selected-change')
 
+  let gesture = 'click'
+    , selected = []
+    , selectedRowsByColumnId = {}
+
   function cellSelection(s) {
     s.each(cellSelectionEach)
+  }
+
+  cellSelection.selected = function(value) {
+    if (!arguments.length) return selected
+    selected = value
+    return cellSelection
   }
 
   return rebind().from(dispatch, 'on')(cellSelection)
 
   function cellSelectionEach(d, i) {
-    console.debug('cellSelectionEach', d, this)
+
+    const target = select(this)
+
+    console.info('CONSOLIDATE SELECTED CELLS')
+
+    d.dispatcher
+        .on('cell-enter.cell-selection', onCellEnter)
+        .on('cell-update.cell-selection', onCellUpdate)
+  }
+
+  function onCellEnter(d, i) {
+    select(this).on('click.cell-selection', onClick)
+  }
+
+  function onCellUpdate(d, i){
+    select(this).classed('is-selected', isCellSelected)
+  }
+
+  function isCellSelected(d, i) {
+    return Math.random() > 0.5
+  }
+
+  function onClick(d, i) {
+    console.debug('onClick cell select', d.column.id, d.row)
   }
 }
