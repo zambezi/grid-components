e4a18b52c6ef9bbff0df883a17385b060686fe81
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-09-23 11:49:57 +0100

    Implement range selection

--- src/cell-selection.js
+++ src/cell-selection.js
@@ -1,6 +1,6 @@
 import { dispatch  as createDispatch }  from 'd3-dispatch'
 import { rebind, forward } from '@zambezi/d3-utils'
-import { reduce, indexBy, findWhere } from 'underscore'
+import { reduce, indexBy, findIndex, range } from 'underscore'
 import { select, event } from 'd3-selection'
 import { unwrap } from '@zambezi/grid'
 
@@ -36,12 +36,12 @@ export function createCellSelection() {
 
   return rebind().from(dispatch, 'on')(cellSelection)
 
-  function cellSelectionEach(rows, i) {
+  function cellSelectionEach(bundle, i) {
     const target = select(this)
 
     if (selectedCandidates) updateFromCandidates()
 
-    rows.dispatcher
+    bundle.dispatcher
         .on('cell-enter.cell-selection', onCellEnter)
         .on('cell-update.cell-selection', onCellUpdate)
 
@@ -53,7 +53,7 @@ export function createCellSelection() {
 
     function toRealSelection(acc, { row, column }) {
       const columnFound = typeof column == 'string' ? columnById[column] : column
-          , rowFound = typeof row == 'number' ? rows[row] : row
+          , rowFound = typeof row == 'number' ? bundle[row] : row
 
       if (!columnFound || !rowFound) {
         console.warn("Couldn't find cell for", { row, column })
@@ -69,7 +69,7 @@ export function createCellSelection() {
     }
 
     function compileSelected() {
-      columnById = indexBy(rows.columns, 'id')
+      columnById = indexBy(bundle.columns, 'id')
       return reduce(selectedRowsByColumnId, toCells, [])
     }
 
@@ -82,18 +82,29 @@ export function createCellSelection() {
           , columnId = column.id
           , set = selectedRowsByColumnId[columnId]
           , row = unwrap(d.row)
-          , shift = event.shiftKey
-          , ctrl = event.ctrlKey
+          , { shiftKey, ctrlKey } = event
           , target = { row, column }
           , isAlreadySelected = set && set.has(row)
+          , hasActive = !!active
 
       switch(true) {
-        case ctrl && isAlreadySelected: 
+
+        case ctrlKey && shiftKey && hasActive:
+          addRangeToSelection(active, target)
+          break
+
+        case shiftKey && hasActive:
+          setSelectionToRange(active, target)
+          break
+
+        case ctrlKey && isAlreadySelected:
           removeFromSelected(target)
           break
-        case ctrl: 
+
+        case ctrlKey:
           addToSelected(target)
           break
+
         default:
           selectOnly(target)
       }
@@ -105,14 +116,51 @@ export function createCellSelection() {
       select(this).dispatch('redraw', { bubbles: true })
     }
 
+    function setSelectionToRange(a, b) {
+      selectedRowsByColumnId = {}
+      rangeFrom(a, b).forEach(addToSelected)
+    }
+
+    function addRangeToSelection(a, b) {
+      rangeFrom(a, b).forEach(addToSelected)
+    }
+
+    function rangeFrom(a, b) {
+      const columns = bundle.columns
+          , columnRange = rangeFromUnorderedIndices(
+              columns
+            , columns.indexOf(a.column)
+            , columns.indexOf(b.column)
+            )
+          , rows = bundle.rows
+          , rowRange = rangeFromUnorderedIndices(
+              rows
+            , findIndex(rows, r => unwrap(r) == a.row)
+            , findIndex(rows, r => unwrap(r) == b.row)
+            ).map(unwrap)
+
+      return rowRange.reduce(allCellsInColumns, [])
+
+      function allCellsInColumns(acc, row) {
+        return acc.concat(columnRange.map(column => ({ column, row })))
+      }
+    }
+
     function selectOnly(target) {
       selectedRowsByColumnId = {}
       addToSelected(target)
     }
 
+    function rangeFromUnorderedIndices(array, i1, i2) {
+      return array.slice(
+        Math.min(i1, i2)
+      , Math.max(i1, i2) + 1
+      )
+    }
+
     function removeFromSelected({ row, column }) {
       const columnId = column.id
-          , set = selectedRowsByColumnId[columnId] 
+          , set = selectedRowsByColumnId[columnId]
 
       if (set) set.delete(row)
     }
