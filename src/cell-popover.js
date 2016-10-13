9bf81656e00dbca3fcabb1d64f019fd57982941e
Cristiano Belloni <cristiano.belloni@icap.com>
2016-10-13 14:36:08 +0100

    Leave the new popover open if a new handle is clicked

--- src/cell-popover.js
+++ src/cell-popover.js
@@ -7,6 +7,7 @@ import './cell-popover.css'
 export function createCellPopover() {
 
   const dispatch = createDispatch('open', 'close')
+  let lastCreated
 
   function cellPopover(d, i) {
     select(this)
@@ -29,17 +30,19 @@ export function createCellPopover() {
               .style('left', `${left + width}px`)
               .classed('is-open', true)
 
+    lastCreated = clickCloseEventName
+
     dispatch.call('open', popover.node(), d)
     select('.info-box pre').text(`Created popover for ${d.row.name}`)
     body.on(clickCloseEventName, onClose)
 
     function onClose() {
-      console.log('yo')
       const target = event.target
       if (popover.node().contains(target)) return
       if (button.contains(target)) return
       body.on(clickCloseEventName, null)
       event.stopPropagation()
+      if (lastCreated !== clickCloseEventName) return
       popover.remove()
       dispatch.call('close', popover.node())
     }
