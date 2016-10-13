9584cf92fc7399946dea6abc4df949684d4736fd
Cristiano Belloni <cristiano.belloni@icap.com>
2016-10-13 11:51:45 +0100

    Open the popover in the right (absolute) position

--- src/cell-popover.js
+++ src/cell-popover.js
@@ -1,23 +1,36 @@
 import { appendIfMissing } from '@zambezi/d3-utils'
-import { select } from 'd3-selection'
+import { select, event } from 'd3-selection'
+import { uniqueId } from 'underscore'
 import './cell-popover.css'
 
 export function createCellPopover() {
+
+  const eventType = uniqueId('click.cell-popover-')
 
   function cellPopover(d, i) {
     select(this)
       .select(appendIfMissing('button.custom-element'))
         .text('✓')
-        .on('click.custom-component-click', createPopover)
+        .on('click.cell-popover', createPopover)
   }
 
   return cellPopover
 
   function createPopover(d) {
-    select('.info-box p').text(`Created popover for ${d.row.name}.`)
-    const popover = select(document.body)
+
+    const target = event.target
+        , { top, left, width, height } = target.getBoundingClientRect()
+        , popover = select(document.body)
             .select(appendIfMissing('div.zambezi-cell-popover'))
+              .style('top', `${Math.floor(top + height / 2)}px`)
+              .style('left', `${left + width}px`)
               .classed('is-open', true)
               .text(`Je suis ${d.row.name}!`)
+
+    select('.info-box pre').text(`Created popover for ${d.row.name}
+Cell bounding rect is
+${JSON.stringify({ top, left })}`)
+
+    select(document.body).on(eventType, () => console.log(`${eventType}!`))
   }
 }
