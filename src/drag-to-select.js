8c5dca926c9417da0e145d2f899a1bf21b6a2aeb
Gabriel Montagné <Gabriel.Montagne@icap.com>
2017-01-26 17:01:16 +0000

    Set up drag events on cell.

--- src/drag-to-select.js
+++ src/drag-to-select.js
@@ -1,13 +1,24 @@
-import { drag } from 'd3-drag'
+import { drag as createDrag } from 'd3-drag'
+import { select } from 'd3-selection'
 
 export function createDragToSelect() {
 
-
-  console.log(drag())
+  const drag = createDrag()
+            .on('start.log', d => console.log('START', d))
+            .on('end.log', d => console.log('END', d))
 
   function dragToSelect(s) {
-    s.call(drag)
+    s.each(dragToSelectEach)
   }
 
   return dragToSelect
+
+  function dragToSelectEach({ dispatcher }) {
+    dispatcher.on('cell-enter.log', d => console.log('on cell enter', d))
+        .on('cell-enter.drag-to-select', setupDragEvents)
+  }
+
+  function setupDragEvents() {
+    select(this).call(drag)
+  }
 }
