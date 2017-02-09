6d6ea212e678734543fe474c93ac3001559ba1f4
Gabriel Montagné <Gabriel.Montagne@icap.com>
2017-02-09 12:22:17 +0000

    Determine cell borders.

--- src/highlight-selected-cells.js
+++ src/highlight-selected-cells.js
@@ -1,4 +1,5 @@
 import { select } from 'd3-selection'
+import { findIndex } from 'underscore'
 
 export function createHighlightSelectedCells() {
 
@@ -18,30 +19,69 @@ export function createHighlightSelectedCells() {
 
   return highlightSelectedCells
 
-  function highlightSelectedCellsEach(d) {
+  function highlightSelectedCellsEach(bundle) {
     const target = select(this)
               .on(
                 'data-dirty.highlight-selected-cells',
                 () => borderCache = null
               )
+        , { columns, rows } = bundle
 
     if (!borderCache)  {
       console.log('compile border cache')
       borderCache = compileBorderLayout()
     }
-  }
 
-  function compileBorderLayout() {
-    const rowToIndex = new Set()
-    const columnToIndex = new Set()
+    console.log('render border cache!', borderCache)
 
-    const result = selectedCells.reduce(toCoords, [])
+    function compileBorderLayout() {
 
-    function toCoords(cell) {
-      console.log('To coords', cell)
+      const rowToIndex = new Map()
+          , columnToIndex = new Map()
+          , selectedCellGrid = []
+
+      return selectedCells
+          .reduce(toCoords, [])
+          .reduce(toBorderCells, [])
+          .filter(hasAnyBorder)
+
+      function toCoords(acc, { row, column }) {
+        let columnIndex = columnToIndex.has(column) ? 
+                columnToIndex.get(column) : findIndex(columns, column)
+          , rowIndex = rowToIndex.has(row) ?
+                rowToIndex.get(row) : findIndex(rows, row)
+          , gridRow = selectedCellGrid[rowIndex] || []
+          , cell = { row, column, rowIndex, columnIndex }
+
+        gridRow[columnIndex] = cell
+
+        columnToIndex.set(column, columnIndex)
+        rowToIndex.set(row, rowIndex)
+        selectedCellGrid[rowIndex] = gridRow
+        acc.push(cell)
+        return acc
+      }
+
+      function toBorderCells(acc, cell) {
+        const { rowIndex, columnIndex } = cell
+            , hasLeftBorder = !selectedCellGrid[rowIndex][columnIndex - 1]
+            , hasRightBorder = !selectedCellGrid[rowIndex][columnIndex + 1]
+            , hasTopBorder = !selectedCellGrid[rowIndex - 1] 
+                  || !selectedCellGrid[rowIndex -1][columnIndex]
+            , hasBottomBorder = !selectedCellGrid[rowIndex + 1] 
+                  || !selectedCellGrid[rowIndex + 1][columnIndex]
+            , borderCell = Object.assign(
+                cell
+              , { hasLeftBorder, hasRightBorder, hasTopBorder, hasBottomBorder }
+              )
+
+        if (hasAnyBorder(borderCell)) acc.push(borderCell)
+        return acc
+      }
+
+      function hasAnyBorder({ hasLeftBorder, hasRightBorder, hasTopBorder, hasBottomBorder }) {
+        return hasLeftBorder || hasRightBorder || hasTopBorder || hasBottomBorder
+      }
     }
-
-    return {}
-
   }
 }
