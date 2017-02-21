3f0e5f40a8a63903c4b640b55ca1746aba444375
Gabriel Montagné <Gabriel.Montagne@icap.com>
2017-02-21 15:28:10 +0000

    Remove empty paste behaviour stub.

--- src/paste-behaviour.js
+++
@@ -1,9 +0,0 @@
-import { dispatch as createDispatch } from 'd3-dispatch'
-
-export function createPasteBehaviour() {
-  function pasteBehaviour(s) {
-    console.log('pate behaviour', s.node())
-  }
-
-  return pasteBehaviour
-}
