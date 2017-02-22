8c069c42809314541857584b32ab21b66ea3c425
Gabriel Montagné <Gabriel.Montagne@icap.com>
2017-02-22 17:15:32 +0000

    Display active row.

--- src/row-selection.js
+++ src/row-selection.js
@@ -1,19 +1,47 @@
-import { dispatch  as createDispatch }  from 'd3-dispatch'
 import { rebind, forward } from '@zambezi/d3-utils'
+import { select, event } from 'd3-selection'
+import { unwrap } from '@zambezi/grid'
 
 import './row-selection.css'
 
 export function createRowSelection() {
 
-  const dispatch = createDispatch('row-selected-change')
+  let active
 
   function rowSelection(s) {
     s.each(rowSelectionEach)
   }
 
-  return rebind().from(dispatch, 'on')(rowSelection)
+  rowSelection.selected = function(value) {
+    if (!arguments.length) return selected
+    selected = value
+    return rowSelection
+  }
 
-  function rowSelectionEach(d) {
-    console.log('rowSelectionEach', this, d)
+  return rowSelection
+
+  function rowSelectionEach({ dispatcher }) {
+    dispatcher
+        .on('row-enter.row-selection', attachListeners)
+        .on('row-update.row-selection', updateRow)
+        .on('row-exit.row-selection', removeListeners)
+  }
+
+  function updateRow({ row }) {
+    const isActive = unwrap(row) === active
+    select(this).classed('is-active', isActive)
+  }
+
+  function attachListeners() {
+    select(this).on('click.row-selection', setActive)
+  }
+
+  function removeListeners() {
+    select(this).on('click.row-selection', null)
+  }
+
+  function setActive({ row }) {
+    active = unwrap(row)
+    select(this).dispatch('redraw', { bubbles: true })
   }
 }
