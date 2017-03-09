9a9fb28c11f71fb9ca8f3c72df634467f4983baa
Gabriel Montagné <Gabriel.Montagne@icap.com>
2017-03-09 14:10:27 +0000

    Fix headers update.

--- src/crosshairs.js
+++ src/crosshairs.js
@@ -26,54 +26,55 @@ export function createCrosshairs() {
           .on('row-enter.crosshairs-row', horizontal ? setRowListeners : null)
           .on('row-update.crosshairs-row', horizontal ? updateRowHighlight : null)
           .on('row-exit.crosshairs-row', horizontal ? clearRowListeners : null)
-  }
 
-  function updateHeaders() {
-    target.selectAll('.zambezi-grid-headers .zambezi-grid-header')
-          .each(updateHeaderHighlight)
-  }
+    function updateHeaders() {
+      target.selectAll('.zambezi-grid-headers .zambezi-grid-header')
+            .on('mouseover.crosshairs-column', vertical ? column => onColumnHover({ column }) : null)
+            .each(updateHeaderHighlight)
+    }
 
-  function onRowHover({ row }) {
-    highlightedRow = row
-    target.selectAll('.zambezi-grid-row')
-        .each(updateRowHighlight)
-  }
+    function onRowHover({ row }) {
+      highlightedRow = row
+      target.selectAll('.zambezi-grid-row')
+          .each(updateRowHighlight)
+    }
 
-  function onColumnHover({ column }) {
-    highlightedColum = column
-    target.selectAll('.zambezi-grid-cell')
-        .each(updateColumnHighlight)
+    function onColumnHover({ column }) {
+      highlightedColum = column
+      target.selectAll('.zambezi-grid-cell')
+          .each(updateColumnHighlight)
 
-    updateHeaders()
-  }
+      updateHeaders()
+    }
 
-  function setRowListeners(d) {
-    select(this).on('mouseover.crosshairs-row', onRowHover)
-  }
+    function setRowListeners(d) {
+      select(this).on('mouseover.crosshairs-row', onRowHover)
+    }
 
-  function updateRowHighlight({ row }) {
-    select(this).classed('is-crosshairs-over', row === highlightedRow)
-  }
+    function updateRowHighlight({ row }) {
+      select(this).classed('is-crosshairs-over', row === highlightedRow)
+    }
 
-  function updateColumnHighlight({ column }) {
-    select(this).classed('is-crosshairs-over', column === highlightedColum)
-  }
+    function updateColumnHighlight({ column }) {
+      select(this).classed('is-crosshairs-over', column === highlightedColum)
+    }
 
-  function setColumnListeners(d) {
-    select(this).on('mouseover.crosshairs-row', onColumnHover)
+    function setColumnListeners(d) {
+      select(this).on('mouseover.crosshairs-row', onColumnHover)
 
-  }
+    }
 
-  function clearRowListeners(d) {
-    select(this).on('mouseover.crosshairs-row', null)
-  }
+    function clearRowListeners(d) {
+      select(this).on('mouseover.crosshairs-row', null)
+    }
 
-  function clearColumnListeners(d) {
-    select(this).on('mouseover.crosshairs-row', null)
-  }
+    function clearColumnListeners(d) {
+      select(this).on('mouseover.crosshairs-row', null)
+    }
 
-  function updateHeaderHighlight(column) {
-    select(this).classed('is-crosshairs-over', column === highlightedColum)
+    function updateHeaderHighlight(column) {
+      select(this).classed('is-crosshairs-over', column === highlightedColum)
+    }
   }
 
 }
