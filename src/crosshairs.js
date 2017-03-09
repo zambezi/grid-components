312c67b400ca7abcea42826517e567d7e12e09cd
Gabriel Montagné <Gabriel.Montagne@icap.com>
2017-03-09 10:47:26 +0000

    Basic row highlgiht implementation.

--- src/crosshairs.js
+++ src/crosshairs.js
@@ -1,7 +1,10 @@
-
+import { select } from 'd3-selection'
 import './crosshairs.css'
 
 export function createCrosshairs() {
+  
+  let vertical = false
+    , horizontal = true
 
   function crosshairs(s) {
     s.each(crosshairsEach)
@@ -9,8 +12,37 @@ export function createCrosshairs() {
 
   return crosshairs
 
-  function crosshairsEach(d, i) {
-    console.log('crosshairsEach', this, d)
+  function crosshairsEach({ rows, dispatcher }, i) {
+    const target = select(this)
+
+    dispatcher
+          .on('cell-enter.crosshairs-column', vertical ? setColumnListeners : null)
+          .on('cell-exit.crosshairs-column', vertical ? clearColumnListeners : null)
+          .on('row-enter.crosshairs-row', horizontal ? setRowListeners : null)
+          .on('row-exit.crosshairs-row', horizontal ? clearRowListeners : null)
+  }
+
+  function onRowHover(d) {
+    target.selectAll('.zambezi-grid-row')
+        .classed('is-crosshairs-over', r => d.index === r.index)
+        .each(r => console.log(r.index === d.index))
+  }
+
+  function setRowListeners(d) {
+    select(this).on('mouseover.crosshairs-row', onRowHover)
+  }
+
+  function setColumnListeners(d) {
+    select(this).on('mouseover.crosshairs-row', d => console.log('column hover', d, this))
+
+  }
+
+  function clearRowListeners(d) {
+    select(this).on('mouseover.crosshairs-row', null)
+  }
+
+  function clearColumnListeners(d) {
+    select(this).on('mouseover.crosshairs-row', null)
   }
 
 }
